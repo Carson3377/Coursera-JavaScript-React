e325f03ea2efa1ea4b4f77d9dfdf071ac2d25112
Carson'L <105184379+Carson3377@users.noreply.github.com>
2022-12-28 20:54:55 +0800

    Very Reactive!

---
+++ reactlab/src/App.js
@@ -0,0 +1,63 @@
+import {useState, useRef} from "react"; 
+import "./App.css";
+
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) {
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+ 
+  function times(e) { 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));  
+  }; 
+ 
+  function divide(e) {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+ 
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  }; 
+ 
+  function resetResult(e) {
+    e.preventDefault();
+    setResult((prevValue) => prevValue * 0);
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Substract</button>
+        <button onClick={times}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
